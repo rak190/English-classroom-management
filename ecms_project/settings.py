001ad421039af33fd01b1d4c5533525bdcdc7a7e
rak190 <rak190@users.noreply.github.com>
2026-06-24 08:16:58 +0700

    Fix WhiteNoise 500 Error due to missing vendor static file

--- ecms_project/settings.py
+++ ecms_project/settings.py
@@ -128,6 +128,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+WHITENOISE_MANIFEST_STRICT = False
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
